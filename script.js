cdfe1304afb54a722686a6e6b8fcb2945fd2d51d
Camille-lt <camillelebigot5@gmail.com>
2025-03-11 10:05:02 +0100

    final commit

---
+++ script.js
@@ -0,0 +1,24 @@
+async function fetchOffers() {
+    const response = await fetch("https://www.codepassport.dev/api/offers");
+    // console.log(response);
+    const offers = await response.json();
+    
+    
+    for (let i =0; i < offers.length; i++) {
+        // console.log(offers[i].titre) // affiche les titres
+        // console.log(offers[i].description) // affiche les description
+
+        // console.log(offers[i]) 
+        const elementTitle= document.createElement("h2"); // titres
+        elementTitle.innerHTML = offers[i].titre;
+
+        const elementDescriptions= document.createElement("p"); // description
+        elementDescriptions.innerHTML = offers[i].description;
+
+        document.querySelector('#offers-container').appendChild(elementTitle);
+        document.querySelector('#offers-container').appendChild(elementDescriptions);
+    }
+};
+fetchOffers();
+
+
